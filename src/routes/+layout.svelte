8b6e66c74b53b9a9fcfc8c11f0249af1fd5ace3a
ergalla <sokuciusergalla@gmail.com>
2023-07-08 23:18:45 +0500

    prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <div class="container">
-  <!-- Pages will be injected below -->
-  <slot></slot>  
+	<!-- Pages will be injected below -->
+	<slot />
 </div>
 
 <style>
-  .container {
-    max-width: 1400px;
-    margin: 50px auto;
-  }
+	.container {
+		max-width: 1400px;
+		margin: 50px auto;
+	}
 </style>
